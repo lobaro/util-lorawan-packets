413fbe2effb53c201cd9a6afb554564007ecd20e
Tobias Rohde <tr@lobaro.com>
2017-10-31 20:54:52 +0100

    fix comment

--- lw_packets.h
+++ lw_packets.h
@@ -165,8 +165,8 @@ typedef struct {
 // these parameters should be known by your LoRaWAN stack which uses this lib
 // maybe a simple wrapper is needed
 typedef struct {
-	Lorawan_netCtrl_t* pNetCtrl; // pointer to external App netCtrl structure (see lw_const.h)
-	Lorawan_devCfg_t* pDevCfg;  // pointer to external App devCtrl structure (see lw_const.h)
+	Lorawan_netCtrl_t* pNetCtrl; // pointer to external App netCtrl structure (see lw_state.h)
+	Lorawan_devCfg_t* pDevCfg;  // pointer to external App devCtrl structure (see lw_state.h)
 } lwPackets_state_t;
 
 // Setup external dependencies
